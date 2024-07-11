3706a6feb3aefb162d1f34f86c9e8c9a1e80336a
Punit30 <punit.s@consultadd.com>
2024-07-11 19:39:08 +0530

    ADDED: android call after incoming

--- android/src/main/kotlin/com/twilio/twilio_voice/TwilioVoicePlugin.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/TwilioVoicePlugin.kt
@@ -509,7 +509,7 @@ class TwilioVoicePlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamH
                 val activeCallHandle = TVConnectionService.getActiveCallHandle();
                 if(activeCallHandle != null){
                     val activeCall = TVConnectionService.getConnection(activeCallHandle);
-                    result.success(activeCall?.twilioCall?.toString())
+                    result.success(activeCall?.getCallParameters().toString())
                 } else {
                     result.success(null)
                 }
